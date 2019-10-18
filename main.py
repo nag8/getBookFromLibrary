a9cd83b59c2449db9c46aae2574d1ededff3bdea
Ken Nagamine <7955031+nag8@users.noreply.github.com>
2019-10-18 16:21:39 +0900

    2019/10/18作業分

--- main.py
+++ main.py
@@ -3,23 +3,18 @@ from selenium.webdriver.chrome.options import Options
 import configparser
 import urllib.request
 
-# 設定ファイル取得
 def getIniFile():
     iniFile = configparser.ConfigParser()
     iniFile.read('./config.ini', 'UTF-8')
     return iniFile
 
-# chromeを準備
 def initDriver(iniFile):
 
-    # driverを定義
     options = webdriver.ChromeOptions()
 
-    #プロファイル場所を定義
     options.add_argument('--user-data-dir=' + iniFile.get('settings', 'user-data-dir'))
     return webdriver.Chrome(options=options)
 
-# 写真IDのリストを取得
 def getKeyList(iniFile):
     keyList = []
     with open(iniFile.get('settings', 'READFILE'),'r') as f:
@@ -30,17 +25,13 @@ def getKeyList(iniFile):
 
     return keyList
 
-# メイン処理
 def main():
 
-    # 設定ファイル取得
     iniFile = getIniFile()
 
-    # chrome画面を準備
     driver = initDriver(iniFile)
     
 
-    # 終了
     driver.quit()
     exit()
 
