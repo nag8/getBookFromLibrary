a8eb18da1c957ff395f7827d67c94f7b2fd5cfa6
Ken Nagamine <7955031+nag8@users.noreply.github.com>
2019-10-18 22:20:03 +0900

    2019/10/18作業分

--- README.md
+++ README.md
@@ -5,9 +5,12 @@
 - 大阪市図書館を想定
 
 ## 方法
-1. 読書メーターの「[読みたい本](https://bookmeter.com/users/763253/books/wish)」から情報を[google spreadsheet](https://docs.google.com/spreadsheets/d/1xH_mE_R2g9L9QAUx8BVr_LuxhGQX9Ydwm9YofEXjkNg/edit?usp=sharing)に転記
-2. google spreadsheetの未予約のものを予約
+1. 読書メーターの「[読みたい本](https://bookmeter.com/users/763253/books/wish)」から情報を[firebase](https://console.firebase.google.com/u/0/project/bookbook-eaf17/database/bookbook-eaf17/data)に転記
+2. firebaseの未予約のものを予約
 
 ## memo
 ### goLang
 https://qiita.com/kou_pg_0131/items/dab4bcbb1df1271a17b6
+
+### python
+https://github.com/nag8/mixiPhotoDL/blob/master/mixi.py
--- main.py
+++ main.py
@@ -2,39 +2,51 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import configparser
 import urllib.request
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import db
+
+iniFile      = ''
+bookList     = []
+
+def main():
+
+    iniFile = getIniFile()
+    driver = initDriver(iniFile)
+    
+    driver.get('https://www.oml.city.osaka.lg.jp/?page_id=266')
+    
+
+    driver.quit()
+    exit()
+
+class Book():
+
+    def __init__(self, name, status):
+        self.name  = name
+        self.status  = status
 
 def getIniFile():
     iniFile = configparser.ConfigParser()
-    iniFile.read('./config.ini', 'UTF-8')
+    iniFile.read('./config/config.ini', 'UTF-8')
     return iniFile
 
 def initDriver(iniFile):
 
     options = webdriver.ChromeOptions()
 
-    options.add_argument('--user-data-dir=' + iniFile.get('settings', 'user-data-dir'))
+    options.add_argument('--user-data-dir=' + iniFile.get('settings', 'USER_DIR'))
     return webdriver.Chrome(options=options)
 
-def getKeyList(iniFile):
-    keyList = []
+def getBookList(iniFile):
+    global bookList
     with open(iniFile.get('settings', 'READFILE'),'r') as f:
         line = f.readline()
         while line:
-            keyList.append(line.strip())
+            bookList.append(line.strip())
             line = f.readline()
-
-    return keyList
-
-def main():
-
-    iniFile = getIniFile()
-
-    driver = initDriver(iniFile)
-    
-
-    driver.quit()
-    exit()
-
+    Book
+    return bookList
 
 if __name__ == '__main__':
     main()
