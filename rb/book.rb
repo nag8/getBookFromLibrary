2ea7db910cce83d6c0e38b075e5021dfae989546
Ken Nagamine <7955031+nag8@users.noreply.github.com>
2019-10-26 09:37:57 +0900

    ruby分化

--- rb/book.rb
+++
@@ -1,6 +0,0 @@
-class Book(object):
-    def __init__(self, title, auther, readed_date, page):
-        self.title = title
-        self.auther = auther
-        self.readed_date = readed_date
-        self.page = page
---
+++ src/book.rb
@@ -0,0 +1,7 @@
+class Book(object):
+    def __init__(self, title, id):
+        self.title = title
+        self.id = id
+        # self.auther = auther
+        # self.readed_date = readed_date
+        # self.page = page
