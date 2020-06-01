30766e20a7b5b0a3546e683f0fbc88254461c427
Ken Nagamine <7955031+nag8@users.noreply.github.com>
2020-06-01 15:38:10 +0900

    修正

--- src/main.py
+++ src/main.py
@@ -3,20 +3,17 @@ import bookMeter
 import library
 
 def main():
-    
+
     # updateFirebase()
     books = firebase.readBooks()
     for book in books:
         library.book(book)
         firebase.update(book)
-        
-    
-    
+
 def updateFirebase():
     books = bookMeter.getBooks()
     for book in books:
         firebase.addBook(book)
-    
     
 if __name__ == '__main__':
     main()
